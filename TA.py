b0704c99784d7b6c3c8e943ace9934f86151a306
zrezke <jeretina.filip@gmail.com>
2021-03-20 23:22:41 +0100

    simple moving average

--- TA.py
+++ TA.py
@@ -1,1 +1,19 @@
 import math
+
+class TA:
+    def __init__(self, data):
+        self.data = data
+        self.closePriceSum = sum(map(lambda x: float(x[4]), data))
+        self.numberOfPeriods = len(data)
+    
+    def simple_moving_average(self):
+        return self.closePriceSum / self.numberOfPeriods
+
+    def stoch_relative_strength_index(self):
+        pass
+    
+    def boiler_bands(self):
+        pass
+
+    def moving_average_convergence_divergence(self):
+        pass
