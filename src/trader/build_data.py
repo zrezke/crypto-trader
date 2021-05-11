de5f76deb15a3e00bc1976ca385c031aa6c32049
zrezke <jeretina.filip@gmail.com>
2021-05-11 15:27:47 +0200

    added build_data.py

---
+++ src/trader/build_data.py
@@ -0,0 +1,14 @@
+import ta
+import pandas as pd
+import requests
+
+
+def build(kline_data) -> pd.DataFrame:
+    """
+    :param: kline_data `json`
+    """
+    data = [[float(val) for val in item] for item in kline_data]
+
+    df = pd.DataFrame(data, columns=["Timestamp", "Open", "High",
+    "Low", "Close", "Volume", "close time", "qav", "n of trades", "tbbav", "tbqav", "ignore"])
+    return ta.add_all_ta_features(df, "Open", "High", "Low", "Close", "Volume")
--- src/trader/tests.py
+++ src/trader/tests.py
@@ -1,4 +1,11 @@
 import api
+import requests
+import ta
+import pandas as pd
+
+BASE_URL = "https://api.binance.com/"
+SERVER_TIME_ENDPOINT = BASE_URL + "api/v3/time"
+CANDLE_DATA_ENDPOINT = BASE_URL + "api/v3/klines"
 api_class = api.Api()
 def api_security_test():
     params = "symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTC&quantity=1&price=0.1&recvWindow=5000&timestamp=1499827319559"
@@ -20,4 +27,27 @@ def api_order_test():
     resp = api_class.make_new_order(kwargs)
     print(resp)
 
-api_order_test()
+def ta_df_test():
+    serverTime = requests.get(SERVER_TIME_ENDPOINT).json()['serverTime']
+
+    DATA_LIMIT = 518
+    args = {
+        "symbol": "ADABTC",
+        "interval": "1m",
+    }
+
+    request = requests.get(CANDLE_DATA_ENDPOINT, params=args)
+    data = request.json()
+    data = [[float(val) for val in item] for item in data]
+
+    df = pd.DataFrame(data, columns=["Timestamp", "Open", "High",
+    "Low", "Close", "Volume", "close time", "qav", "n of trades", "tbbav", "tbqav", "ignore"])
+    allIndicators = ta.add_all_ta_features(df, "Open", "High", "Low", "Close", "Volume")
+    print(allIndicators)
+    allIndicators.to_csv('indicators.csv')
+    '''
+    rsi = ta.momentum.rsi(df["Close"], fillna=True)
+    sRSI = ta.momentum.stochrsi(df["Close"])
+    print(rsi.iloc[-1], sRSI.iloc[-1])
+    '''
+ta_df_test()
